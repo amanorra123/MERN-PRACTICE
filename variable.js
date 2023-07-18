231c0bfa8f9ce982d4f36825dcb3148960f653bf
Aman Choudhary <amanorra0909@gmail.com>
2023-07-18 11:47:20 +0530

    add.variable.js

--- variable.js
+++ variable.js
@@ -1,5 +1,22 @@
 // var x=6;
 // console.log(x);
 
-var isnull=null;
-console.log(isnull+5);
+// var isnull=null;
+// console.log(isnull+5);
+
+// var y= 2+'cat';
+// console.log(typeof y);
+// console.log(y);
+
+// function addNumbers(num1,num2){
+//     var result=num1+num2;
+//     console.log(result);
+// }
+// addNumbers(12,3);
+
+function add(num1,num2){
+    return num1+num2;
+}
+console.log(add(3,4));
+
+

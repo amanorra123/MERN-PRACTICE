9200f53b1d733095b4915e4520c13fcf6c6650f3
Aman Choudhary <amanorra0909@gmail.com>
2023-07-19 09:21:19 +0530

    add.operators.js

---
+++ operators.js
@@ -0,0 +1,11 @@
+var mynumber=28;
+var mynumber2='28';
+console.log(28=='30');
+
+var mynumber=28;
+var mynumber2='28';
+console.log(28==='28');
+
+
+console.log(mynumber!=29);
+console.log(mynumber!==mynumber2);

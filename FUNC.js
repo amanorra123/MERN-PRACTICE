7cc0c636a7ebfc894e13370c200a9177ba8f0911
Aman Choudhary <amanorra0909@gmail.com>
2023-07-25 11:35:14 +0530

    add FUNC.js file

--- FUNC.js
+++ FUNC.js
@@ -8,3 +8,23 @@ console.log(abc);
 
 // var xyz=abc;
 // console.log(xyz);
+
+const array=[1,2,3];
+array.push(4);
+debugger
+console.log(array);
+
+
+// //////////////////////////////////////////////////////////
+ var employee=[
+    
+    {name:"m",age:20},
+    {name:"x",age:30},
+    {name:"z",age:40},
+
+ ]
+ for(let i=0;i<employee.length;i++){
+    employee[i].age+=10;
+ }
+ 
+ console.log(employee)
